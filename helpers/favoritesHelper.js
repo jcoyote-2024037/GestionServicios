06f6590a92b1c02979ffbd28f396832c67e0a86f
jcoyote-2024037 <jcoyote-2024037@kinal.edu.gt>
2026-03-10 17:47:16 -0600

    fix: Se agregan validaciones y campos nuevos para los servicios

--- helpers/favoritesHelper.js
+++ helpers/favoritesHelper.js
@@ -6,5 +6,7 @@ import Service from '../src/fields/services/services.model.js'
 /** Cuenta los favoritos de un servicio y actualiza el campo favoritosCount en Service */
 export const syncFavoritesCount = async (servicioId) => {
     const count = await Favorite.countDocuments({ servicioId })
-    await Service.findByIdAndUpdate(servicioId, { favoritosCount: count })
+    await Service.findByIdAndUpdate(servicioId, {
+        favoritosCount: count, lastActivityAt: new Date()
+    })
 }
