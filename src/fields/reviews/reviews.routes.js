2b147229ef4d76b1f743bd1e961282c4e426840b
pcalderon-2021547 <pcalderon-2021547@kinal.edu.gt>
2026-03-09 23:47:03 -0600

    ADD: proteger rutas con validateJWT y registrar rutas

--- src/fields/reviews/reviews.routes.js
+++ src/fields/reviews/reviews.routes.js
@@ -2,28 +2,22 @@
 
 import { Router } from "express"
 import {
-    createReview,
-    getReviews,
-    getReviewById,
-    getReviewsByService,
-    updateReview,
-    deleteReview
+    createReview, getReviews, getReviewById,
+    getReviewsByService, updateReview, deleteReview,
+    moderateReview, likeReview
 } from "./reviews.controller.js"
-
 import { reviewsValidator, reviewsUpdateValidator } from "../../../middlewares/reviewsValidator.js"
+import { validateJWT } from "../../../middlewares/validate_jwt.js"
 
 const router = Router()
 
-router.post('/create', reviewsValidator, createReview)
-
+router.post('/create', validateJWT, reviewsValidator, createReview)
 router.get('/', getReviews)
-
 router.get('/:id', getReviewById)
-
 router.get('/service/:servicioId', getReviewsByService)
-
-router.put('/update/:id', reviewsUpdateValidator, updateReview)
-
-router.delete('/delete/:id', deleteReview)
+router.put('/update/:id', validateJWT, reviewsUpdateValidator, updateReview)
+router.delete('/delete/:id', validateJWT, deleteReview)
+router.patch('/like/:id', validateJWT, likeReview)
+router.patch('/moderate/:id', validateJWT, moderateReview)
 
 export default router
